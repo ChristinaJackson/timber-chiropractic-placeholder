63120feeafe0db01e743474eb58631c72583e0cc
Christina <chirochristina@gmail.com>
2024-03-24 12:51:21 -0700

    js test

--- testing.js
+++ testing.js
@@ -1,4 +1,18 @@
 
 document.addEventListener('DOMContentLoaded', function () {
      alert("Hello! I am from TimberChiropractic");
+     var target = document.getElementById("jstarget");
+     target.onclick = function() {
+        var randomColor = getRandomColor();
+        target.style.backgroundColor = randomColor;
+    };
    });
+
+   function getRandomColor() {
+    var letters = '0123456789ABCDEF';
+    var color = '#';
+    for (var i = 0; i < 6; i++) {
+      color += letters[Math.floor(Math.random() * 16)];
+    }
+    return color;
+  }
